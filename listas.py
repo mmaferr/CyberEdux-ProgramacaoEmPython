41839f599098e41f5509947a53223be2675b2a09
Maria Fernanda <124887919+mmaferr@users.noreply.github.com>
2023-10-02 08:56:47 -0400

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,21 @@
+itens = [5, 2, 1, 7, 25] #1)
+
+for item in range(15): #2)
+    print(item, 'oi')
+
+    string = "1, 2, 3, 4, 5"
+
+    print(string.split(','))
+
+
+#1) Imagine que você tem uma caixa mágica, chamada "itens", e dentro dessa caixa, você tem algumas coisas especiais,
+#como 5, 2, 1, 7 e 25. Essas são as coisas que você quer mostrar para todos.
+
+#2) Agora, você está prestes a fazer uma apresentação. Você vai falar 15 coisas, uma após a outra,
+#mas você também quer adicionar algo divertido para cada uma delas. Você quer dizer "oi" depois de cada coisa que você mostrar.
+
+#Então, você começa pegando a primeira coisa da sua caixa mágica, que é o número 5. Você a mostra para todos e diz "oi".
+#Agora, você faz algo engraçado: pega uma lista de números de 1 a 5 e a transforma em pedaços, usando vírgulas para separá-los.
+#É como se você estivesse cortando uma pizza em pedaços menores. Você repete isso para todas as 15 coisas que você tem na sua caixa mágica.
+#Então, todas as vezes que você mostrar uma coisa, você diz "oi" e corta uma pizza imaginária em pedaços.
+#No final, você terá uma apresentação com 15 coisas mostradas, acompanhadas de "oi" e algumas pizzas imaginárias cortadas em pedaços.
