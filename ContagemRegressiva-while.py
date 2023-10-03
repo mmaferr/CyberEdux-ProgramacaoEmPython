96bae2763d294f99837061f895fee4610aac2e9e
Maria Fernanda <124887919+mmaferr@users.noreply.github.com>
2023-10-03 09:01:06 -0400

    Add files via upload

---
+++ ContagemRegressiva-while.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um número para iniciar a contagem regressiva: "))
+
+while numero >= 0:
+    print(numero)
+    numero -= 1     # -=  > signica subtrair, é o número menos 1
+
+print("Contagem regressiva concluída!")
