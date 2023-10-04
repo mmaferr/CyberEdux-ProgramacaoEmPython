2e008aa020d3ff53f2c2cae60fba5f736e4126eb
Maria Fernanda <124887919+mmaferr@users.noreply.github.com>
2023-10-04 08:33:05 -0400

    Add files via upload

---
+++ 1a20.py
@@ -0,0 +1,4 @@
+#6. Desenvolva um programa que imprima os números pares de 1 a 20.
+
+for numero in range(2, 21, 2):
+    print(numero)
---
+++ soma1a100.py
@@ -0,0 +1,7 @@
+#7. Faça um programa que calcule a soma dos números de 1 a 100.
+
+soma = 0
+for numero in range(1, 101):
+    soma += numero
+print("A soma dos números de 1 a 100 é:", soma)
+
