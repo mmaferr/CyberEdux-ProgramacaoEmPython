ac2ce0d79353b3c238a2e66b0aac64a6355bc0ff
Maria Fernanda <124887919+mmaferr@users.noreply.github.com>
2023-10-06 11:00:25 -0400

    Add files via upload

--- vogal-ou-consoante.py
+++ vogal-ou-consoante.py
@@ -1,15 +1,9 @@
 #1. Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
-#criar a variável que vai receber a letra digitada
-letra = input("Digite uma letra: ").upper()
-
-# Desenvolvendo a lógica
-if (
-    letra == "A"
-    or letra == "E"
-    or letra == "I"
-    or letra == "O"
-    or letra == "U"
-):
-    print("Vogal")
+print('Olá! Vamos verificar se uma letra é vogal ou consoante!\n')
+letra = input('Digite uma letra: ').upper()
+while len(letra) !=1:
+    letra = input('Você pode digitar apenas uma letra!\nTente novamente: ')
+if letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U':
+    print(f'A letra {letra} é uma Vogal!!!')
 else:
-    print("Consoante")
+    print(f'A letra {letra} é Consoante!!!')
