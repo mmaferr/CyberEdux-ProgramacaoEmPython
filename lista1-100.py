89e8a131e43d505fa89b36c23bde550029df5843
Maria Fernanda <124887919+mmaferr@users.noreply.github.com>
2023-10-18 08:46:42 -0400

    Add files via upload

---
+++ lista1-100.py
@@ -0,0 +1,6 @@
+#Crie uma lista com números de 1 a 10 e imprima-a
+lista_com_numeros = list(range(1, 101))
+print(lista_com_numeros)
+
+#range > gera a sequência uma numérica dentro de um intervalo determinado
+#list > armazena os elementos de forma sequencial e ordenada
