3d3db3db5b0a63f4eaabefe7187e17c81a56626e
Maria Fernanda <124887919+mmaferr@users.noreply.github.com>
2023-10-18 09:15:23 -0400

    Add files via upload

---
+++ lista-multiplique-os-numeros.py
@@ -0,0 +1,9 @@
+#Dada a lista `numeros = [1, 2, 3, 4, 5]`, multiplique cada elemento por 2.
+numeros = [1, 2, 3, 4, 5]
+
+for i in range(len(numeros)):
+    numeros[i] = numeros[i] * 2
+print((numeros))
+
+#range > gera a sequência uma numérica dentro de um intervalo determinado
+#len >  é utilizada para obter o número de itens, contagem
