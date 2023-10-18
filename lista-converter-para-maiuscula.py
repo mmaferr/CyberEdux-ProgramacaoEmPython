1a6568f9c0127ab4de1b7df7b2b15c4dddf72586
Maria Fernanda <124887919+mmaferr@users.noreply.github.com>
2023-10-18 09:45:01 -0400

    Add files via upload

---
+++ lista-converter-para-maiuscula.py
@@ -0,0 +1,7 @@
+#Dada a lista `letras = ['a', 'b', 'c', 'd', 'e']`, converta cada letra para maiúscula.
+
+letras = ['a', 'b', 'c', 'd', 'e']
+lst = [x.upper() for x in letras]
+print(lst)
+
+#upper > converte minúscula para maiúscula
