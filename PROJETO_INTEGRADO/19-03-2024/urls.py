7cfcc8fa01b799a211d6a9978f817e20932efc9c
Maria Fernanda <124887919+mmaferr@users.noreply.github.com>
2024-03-19 09:16:46 -0400

    Add files via upload

---
+++ PROJETO_INTEGRADO/19-03-2024/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from lovelace import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.homepage_view, name='home'),
+    path('login', views.login_view, name='login'),
+    path('cadastrar', views.cadastrar_view, name='cadastrar'),
+    path('sobre', views.sobre_view, name='sobre'),
+    path('linguagens', views.linguagens_view, name='linguagens'),
+    path('cursos', views.linguagens_view, name='cursos'),
+    path('cursos-aluno', views.cursos_aluno_view, name='cursos-aluno'),   
+    path('portal-do-aluno', views.portal_do_aluno_view, name='portal-do-aluno'),   
+    path('meus-cursos', views.meus_cursos_view, name='meus-cursos'), 
+]
